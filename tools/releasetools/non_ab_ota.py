f51cf8c13f12cd251bf1e31c73c1f0b8f971742e
Matt Mower <mowerm@gmail.com>
2014-11-21 00:46:52 -0600

    build: Ensure /system unmounted at install start
    Account for the possibility that a user has mounted system manually
    before installing the FullOTA package.
    
    Change-Id: I97d0540676a7ce98762545e76ecb1a7f6335d204

--- tools/releasetools/non_ab_ota.py
+++ tools/releasetools/non_ab_ota.py
@@ -227,6 +227,7 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
   script.Print("             Presented to you by Ritesh              ")
   script.Print("=====================================================")
 
+  script.AppendExtra("ifelse(is_mounted(\"/system\"), unmount(\"/system\"));")
   device_specific.FullOTA_InstallBegin()
 
   CopyInstallTools(output_zip)
